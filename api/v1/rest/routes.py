7f1f0fed76b08fce8778e5cedc29eb1eecda752d
Vladimir <x55aah@gmail.com>
2019-08-02 16:50:48 +0300

    update

--- api/v1/rest/routes.py
+++ api/v1/rest/routes.py
@@ -1,12 +1,12 @@
 # For more details about routing, see
 # http://www.tornadoweb.org/en/stable/routing.html
 from tornado.web import url
-from . import handlers
+from . import handlers as h
 
 
 route_patterns = [
-    url(r'^/promo/(?P<id>[^/]+)/?$', handlers.PromoCodeHandler, name='promo_code'),
-    url(r'^/promo/?$', handlers.PromoCodeHandler, name='promo_code_create'),
-    url(r'^/promo-list/?$', handlers.PromoCodeListHandler, name='promo_codes'),
-    url(r'^/use-promo/(?P<id>[^/]+)/?$', handlers.UsePromoCode, name='use_promo_code'),
+    url(r'^/promo/(?P<id>[^/]+)/?$', h.PromoCodeHandler, name='promo_code'),
+    url(r'^/promo/?$', h.PromoCodeHandler, name='promo_code_create'),
+    url(r'^/promo-list/?$', h.PromoCodeListHandler, name='promo_codes'),
+    url(r'^/use-promo/(?P<id>[^/]+)/?$', h.UsePromoCode, name='use_promo_code'),
 ]
